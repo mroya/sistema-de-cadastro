537486342055543b78e8d985367a3f982f7879a8
mroya <royamarcio@gmail.com>
2022-07-04 15:27:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento do jogos");
 console.log("aplicação está se conectando ao banco de dados");
+// mais um console log de testes:
+console.log("O Paulo fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
